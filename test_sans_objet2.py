68a9a7a543b533ef26e7e5b62e07628d184054eb
Gazeux33 <gazeux33000@gmail.com>
2023-11-17 23:03:43 +0100

    up

--- test_sans_objet2.py
+++ test_sans_objet2.py
@@ -18,8 +18,8 @@ COLORS = {"-1": "#000000",
 
 class Game:
     def __init__(self):
-        self.NB_CASE = 51
-        self.TILE_SIZE = 12
+        self.NB_CASE = 100
+        self.TILE_SIZE = 20
         self.SIZE = self.NB_CASE * self.TILE_SIZE, self.NB_CASE * self.TILE_SIZE
 
         pygame.init()
@@ -30,7 +30,8 @@ class Game:
 
         self.matrix = [[-1 for y in range(self.NB_CASE)] for i in range(self.NB_CASE)]
         self.add_colors()
-        self.nb_wall_start = len(self.get_walls())
+
+        self.walls = self.get_walls()
 
     def play(self):
         while True:
@@ -39,14 +40,9 @@ class Game:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        for x, y in self.get_tiles():
-                            self.matrix[y][x] = 0
-                    if event.key == pygame.K_i:
-                        self.check_isolated_case()
-            for i in range(50):
-                self.transform_matrix()
-
-
+                        self.transform_matrix()
+            self.transform_matrix()
+            
             self.draw_matrix()
 
             pygame.display.flip()
@@ -86,48 +82,32 @@ class Game:
         return tiles
 
     def transform_matrix(self):
-        walls = self.get_walls()
-        if walls:
-            wall = random.choice(self.get_walls())
-
+        for wall in self.get_walls():
             axis = random.randint(1, 2)
             if axis == 1:
-                self.transform_lines(wall)
+                if not self.transform_lines(wall):
+                    self.transform_columns(wall)
             elif axis == 2:
-                self.transform_columns(wall)
-
-    def check_isolated_case(self):
-        for tile in self.get_tiles():
-            x, y = tile
-            nb_wall = 0
-            if self.matrix[y][x + 1] == -1: nb_wall +=1
-            if self.matrix[y][x - 1] == -1: nb_wall += 1
-            if self.matrix[y - 1][x] == -1: nb_wall += 1
-            if self.matrix[y + 1][x] == -1: nb_wall += 1
-
-            if nb_wall >= 4:
-
-                x_break = random.randint(-1, 1)
-                y_break = random.randint(-1, 1)
-                self.spread_color((x_break, y_break), self.matrix[y][x])
-
+                if not self.transform_columns(wall):
+                    self.transform_lines(wall)
 
     def transform_lines(self, wall):
-
         y, x = wall
         if self.matrix[y][x - 1] != -1 and self.matrix[y][x + 1] != -1:
             if self.matrix[y][x - 1] != self.matrix[y][x + 1]:
-                # self.matrix[y][x] = 0  # => test transform_lines
                 self.spread_color(wall, random.choice([self.matrix[y][x - 1], self.matrix[y][x + 1]]))
-                #  self.matrix[y][x] = random.choice([self.matrix[y][x - 1],self.matrix[y][x + 1]])
+                return True
+        return False
+
 
     def transform_columns(self, wall):
         y, x = wall
         if self.matrix[y - 1][x] != -1 and self.matrix[y + 1][x] != -1:
             if self.matrix[y - 1][x] != self.matrix[y + 1][x]:
-                #  self.matrix[y][x] = 0  # => test transform_columns
                 self.spread_color(wall, random.choice([self.matrix[y - 1][x], self.matrix[y + 1][x]]))
-                #  self.matrix[y][x] = random.choice([self.matrix[y - 1][x],self.matrix[y + 1][x]])
+                return True
+        return False
+
 
     def spread_color(self, wall, color):
         y, x = wall
@@ -141,21 +121,6 @@ class Game:
         if x >= len(self.matrix) - 1 or y >= len(self.matrix) - 1 or x < 0 or y < 0:
             return False
         return True
-
-    def is_finished(self):
-        start = (1, 0)
-        end = (len(self.matrix)-1, len(self.matrix)-2)
-        current_pos = start
-        current_color = self.matrix[start[1]][start[0]]
-
-        def prop(pos, color):
-            x, y = pos
-            for i in range(-1, 2):
-                for j in range(-1, 2):
-                    if abs(i) + abs(j) == 1 and self.is_wall_in_range(y + i, x + j) and self.matrix[y + i][x + j] == color:
-                        new_pos = (y + i, x + j)
-                        prop(new_pos, color)
-                        return new_pos
 
 
 game = Game()
